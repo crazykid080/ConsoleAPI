9bb85413adf0458ecc852afec20947e556cd0e8c
Crazykid080 <orillyt080@gmail.com>
2016-05-24 15:44:47 -0700

    First commit
    Version: 0.0.1

---
+++ ConsoleAPI/src/com/crazykid080/main/adminControls/AdminCommandController.java
@@ -0,0 +1,28 @@
+package com.crazykid080.main.adminControls;
+
+public class AdminCommandController{
+
+	/**
+	 * @author crazykid080
+	 * @since 0.0.1
+	 * @param command the text from the console.
+	 * THIS SHOULD HAVE AN EXTENDED CLASS IN YOUR PROJECT!
+	 */
+	public static void runCommand(String command){		
+		if(command.contains("exit")){
+			exit();
+		}
+		ConsoleControl.write("ERROR: command doesn't seem to be valid!");
+
+	}
+
+
+	/** @author crazykid080
+	 * @since 0.0.1
+	 * Exits the program.
+	 */
+	public static void exit() {
+		System.exit(1);
+	}
+
+}
